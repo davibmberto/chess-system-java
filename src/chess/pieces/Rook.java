a46c3a8d537abdda2a8c0e06e0da9e31f1dc00b2
davibmberto <davi_berto1@hotmail.com.br>
2021-03-12 18:15:50 -0300

    Placing piece on the board

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,20 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece{
+
+	public Rook(Board board, Color color) {
+		//essa parte repassa os dados para o construtor da super classe
+		super(board, color);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "R";
+	}
+
+}
